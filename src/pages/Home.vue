c0d687c2a50f70d6295039cea6b1cd50177885ca
Me <103583267+R4C-Wanz@users.noreply.github.com>
2023-01-26 08:34:03 +0700

    Personal-Website

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="home">
+        <video autoplay loop muted class="bg-video">
+            <source src="../assets/videos/video.mp4" type="video/mp4">
+        </video>
         <div class="content">
             <h1>Jakpus</h1>
             <a href="">Explore</a>
